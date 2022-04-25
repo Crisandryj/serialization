c07e168adc03911f08c37b2ed02b139c60a529ce
Crisandry Javier <crisandryj@gmail.com>
2022-04-25 11:36:28 -0400

    opened files and read every 42nd line

--- additional_prac.rb
+++ additional_prac.rb
@@ -1,16 +1,35 @@
-# # fname = "sample.txt"
-# # somefile = File.open(fname, "w")
-# # somefile.puts "Hello file!"
-# # somefile.close
+# fname = "sample.txt"
+# somefile = File.open(fname, "w")
+# somefile.puts "Hello file!"
+# somefile.close
 
 # File.open("sample.txt","w") {|somefile| somefile.puts "Hello file!"}
 
-require 'rubygems'
-require 'rest-client'
+# require 'rubygems'
+# require 'rest-client'
 
-wiki_url = "http://en.wikipedia.org/"
-wiki_local_filename = "wiki-page.html"
+# wiki_url = "http://en.wikipedia.org/"
+# wiki_local_filename = "wiki-page.html"
 
-File.open(wiki_local_filename, "w") do |file| 
-    file.write(RestClient.get(wiki_url))
-end 
+# File.open(wiki_local_filename, "w") do |file| 
+#     file.write(RestClient.get(wiki_url))
+# end \
+
+#Write a program that:
+
+# Reads hamlet.txt from the given URL
+# Saves it to a local file on your hard drive named "hamlet.txt"
+# Re-opens that local version of hamlet.txt and prints out every 42nd line to the screen
+
+require 'open-uri'
+
+url = "http://ruby.bastardsbook.com/files/fundamentals/hamlet.txt"
+fname = "hamlet.txt"
+
+File.open(fname, "w"){|file| file.write(URI.open(url).read)}
+
+File.open(fname, "r") do |file| 
+  file.readlines.each_with_index do |line,index|
+    puts line if index % 42 == 41
+  end
+end
